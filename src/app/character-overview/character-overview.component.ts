ac86272370175249b01423dbf0c7a55a149a1441
Panzareon <Fogtociag@web.de>
2023-02-25 14:58:15 +0100

    Make values like current hp editable in the character overview

--- src/app/character-overview/character-overview.component.ts
+++ src/app/character-overview/character-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StatsService } from '../stats.service';
 import { CharacterService } from '../character.service';
+import { Character } from '../character';
 
 @Component({
   selector: 'app-character-overview',
@@ -8,10 +9,16 @@ import { CharacterService } from '../character.service';
   styleUrls: ['./character-overview.component.css']
 })
 export class CharacterOverviewComponent implements OnInit {
+  character: Character;
 
   constructor(private statsService : StatsService, public characterService : CharacterService) { }
 
   ngOnInit(): void {
+    this.getCharacter();
+  }
+
+  getCharacter(): void {
+    this.character = this.characterService.getCharacter();
   }
 
   getStats(): string[] {
@@ -20,6 +27,16 @@ export class CharacterOverviewComponent implements OnInit {
 
   getStatValue(stat: string) : number {
     return this.statsService.getStatValue(this.characterService.getCharacter(), stat);
+  }
+
+  canEdit(statName: string) : boolean {
+    var stat = this.statsService.getStat(statName);
+    if (!stat.dependsOn)
+    {
+      return false;
+    }
+
+    return stat.calculate === undefined;
   }
 
   getLabel(stat: string): string {
